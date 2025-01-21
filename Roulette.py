246d72a947734bb62da2ca881582bbf964620911
1cewav3 <1cewav3.official@gmail.com>
2025-01-21 13:57:41 +0100

    Create Roulette.py

---
+++ Roulette.py
@@ -0,0 +1,5 @@
+import random
+print("Welcome to roulette")
+print("In this game you will place a bet on one of four categories.")
+print("After choosing what you will bet on, you will input how much you want to bet.")
+print("Currently you have %d dollars" %money)

e5c036413f4775a2bd36c5538444bc2d416dbd4f
1cewav3 <1cewav3.official@gmail.com>
2025-01-21 14:03:24 +0100

    Create Start.py

---
+++ Start.py
@@ -0,0 +1,15 @@
+print("Welcome to the banking system!")
+print("In this game you manage your own virtual money")
+print("You can gamble you way to richness, or spend it on other stuff")
+print("You currently have %d dollars in the bank" %bank)
+print("And %d dollars in cash" %money)
+print("For the moment, you can only do a few things:")
+print("1. Deposit money")
+print("2. Withdraw money")
+print("3. Gamble")
+choice_1 = input("> ")
+
+
+while True:
+    if choice_1 = 1:
+        print("You chose
